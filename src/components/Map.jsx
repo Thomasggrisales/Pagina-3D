265f1abcaf1e512b35d92981c1b858f010da527a
thomas <thomasggrisales@gmail.com>
2023-04-05 15:23:14 -0500

    Page ready without responsive and whithout 3D elements

---
+++ src/components/Map.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Annotation,
+  ZoomableGroup
+} from "react-simple-maps";
+
+const Map = () => {
+  return (
+    <ComposableMap
+      projection="geoAzimuthalEqualArea"
+      projectionConfig={{
+        rotate: [-10.0, -52.0, 0],
+        center: [-5, -3],
+        scale: 1600
+      }}
+      style={{width:"100%", height:"100%"}}
+    >
+      <Geographies
+        geography="/features.json"
+        fill="#2C065D"
+        stroke="#FFFFFF"
+        strokeWidth={0.5}
+      >
+        {({ geographies }) =>
+          geographies.map((geo) => (
+            <Geography key={geo.rsmKey} geography={geo} />
+          ))
+        }
+      </Geographies>
+      <Annotation
+        subject={[2.3522, 48.8566]}
+        dx={-90}
+        dy={-30}
+        connectorProps={{
+          stroke: "white",
+          strokeWidth: 2,
+          strokeLinecap: "round"
+        }}
+      >
+        <text x="-8" textAnchor="end" alignmentBaseline="middle" fill="white">
+          {"Paris"}
+        </text>
+      </Annotation>
+      <Annotation
+        subject={[21.01178, 52.22977]}
+        dx={-90}
+        dy={-30}
+        connectorProps={{
+          stroke: "white",
+          strokeWidth: 2,
+          strokeLinecap: "round"
+        }}
+      >
+        <text x="-8" textAnchor="end" alignmentBaseline="middle" fill="white">
+          {"Warsaw"}
+        </text>
+      </Annotation>
+      <Annotation
+        subject={[-3.74922, 40.463667]}
+        dx={-90}
+        dy={-30}
+        connectorProps={{
+          stroke: "white",
+          strokeWidth: 2,
+          strokeLinecap: "round"
+        }}
+      >
+        <text x="-8" textAnchor="end" alignmentBaseline="middle" fill="white">
+          {"Spain"}
+        </text>
+      </Annotation>
+      <Annotation
+        subject={[12.56738, 41.87194]}
+        dx={-90}
+        dy={-30}
+        connectorProps={{
+          stroke: "white",
+          strokeWidth: 2,
+          strokeLinecap: "round"
+        }}
+      >
+        <text x="-8" textAnchor="end" alignmentBaseline="middle" fill="white">
+          {"Itily"}
+        </text>
+      </Annotation>
+    </ComposableMap>
+  );
+};
+
+export default Map;
